92fd0c1086579a291895d7edb8aa20345a71d36e
chuba <alinademidova223@gmail.com>
2025-09-16 15:39:24 +0300

    test

--- dz1.java
+++ dz1.java
@@ -1,4 +1,4 @@
-public class Main {
+pupublic class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
 
@@ -9,13 +9,13 @@ public class Main {
         System.out.println("2.5 + 1.2 = " + r2);
 
         int r3 = calc.calculate(1, 2, 3, 4);
-        System.out.println("sum 1,2,3,4 = " + r3);
+        System.out.println("Сумма чисел 1, 2, 3, 4 = " + r3);
 
-        Student s1 = new Student("Ivanov", "Ivan");
-        Student s2 = new Student("Petrova", "Anna");
+        Student s1 = new Student("Иванов", "Иван");
+        Student s2 = new Student("Петрова", "Анна");
 
-        System.out.println("Compare Ivanov vs Petrova (Student): " + s1.compare(s2));
-        System.out.println("Compare Ivanov with \"Petrova\" (String): " + s1.compare("Petrova"));
+        System.out.println("Сравнение студентов Иванов и Петрова (по фамилии): " + s1.compare(s2));
+        System.out.println("Сравнение фамилии Иванов со строкой \"Петрова\": " + s1.compare("Петрова"));
     }
 }
 
@@ -53,10 +53,12 @@ class Student {
         this.firstName = firstName;
     }
 
+    // Сравнение студентов по фамилии
     public int compare(Student other) {
         return this.lastName.compareTo(other.lastName);
     }
 
+    // Сравнение с произвольной фамилией (строкой)
     public int compare(String lastName) {
         return this.lastName.compareTo(lastName);
     }
